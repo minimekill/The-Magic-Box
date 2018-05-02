7561a15824440f70c1ade647c2165bdfb9caf968
cph-jh332 <cph-jh332@cphbusiness.dk>
2018-05-02 15:12:21 +0200

    fixed string to int convertion

--- decision_tree.py
+++ decision_tree.py
@@ -1,20 +1,22 @@
 import json
 
 def distance(x1, x2):
-    return x1 - x2
+    return x2 - x1
 
 def to_jump_or_not_to_jump(x1, x2):
     out_distance = distance(x1, x2)
 
-    data = json.loads(open('jumpingParameter.json'))
+    data = json.loads(open('jumpingParameter.json').read())
 
-    if data['x'] is out_distance:
+    number = int(data['x'])
+
+    if number == out_distance:
         return True
 
     return False
 
 def change_parameter():
-    data = json.loads(open('jumpingParameter.json'))
+    data = json.loads(open('jumpingParameter.json').read())
     x = data['x'] - 1
     jumpingParameter = open('jumpingParameter.json', 'w')
     jumpingParameter.write("{ \"x\": {} }".format(x))
