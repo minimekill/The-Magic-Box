bfdb326be63a4f9a4d5c445c8b20c28dde7bb738
Mikkel <mikkel@mediagram.dk>
2018-05-23 11:29:44 +0200

    header change

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-# KidsCanCode - Game Development with Pygame video series
-# Jumpy! (a platform game) - Part 4
-# Video link: https://youtu.be/G8pYfkIajE8
-# Jumping
+# BloodyTelevision AI Magic Box Adventure
 
 import pygame as pg
 import random
