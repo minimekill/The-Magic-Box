d9e4b89d50c2ccc059796ff9ac0e21ca12042bd1
Joachim Hansen <cph-jh332@cphbusiness.dk>
2018-05-23 18:33:45 +0200

    added middle of upper and lower

--- decision_tree.py
+++ decision_tree.py
@@ -1,13 +1,16 @@
 import decision_tree_lower as dts
 import decision_tree_upper as dtu
+import decision_tree_middle as dtm
 
-mode = "lower"
+mode = "middle"
 
 def to_jump_or_not_to_jump(x1, x2, enemy_height):
     if mode is "upper":
         return dtu.to_jump_or_not_to_jump(x1, x2, enemy_height)
     elif mode is "lower":
         return dts.to_jump_or_not_to_jump(x1, x2, enemy_height)
+    elif mode is "middle":
+        return dtm.to_jump_or_not_to_jump(x1, x2, enemy_height)
 
 def change_parameter(enemy_height):
     if mode is "upper":
---
+++ decision_tree_middle.py
@@ -0,0 +1,33 @@
+import json
+
+
+def distance(x1, x2):
+    return x2 - x1
+
+
+def to_jump_or_not_to_jump(x1, x2, enemy_height):
+    out_distance = distance(x1, x2)
+
+    data_upper = json.loads(open('jumpingParameter.json').read())
+    data_lower = json.loads(open('jumpingParameter2.json').read())
+    if str(enemy_height) in data_upper and str(enemy_height) in data_lower:
+        number_upper = int(data_upper[str(enemy_height)])
+        number_lower = int(data_lower[str(enemy_height)])
+
+        number_accumulated = number_upper - (number_upper - number_lower) / 2
+
+        if out_distance - 3 <= number_accumulated <= out_distance + 3:
+            return True
+
+    return False
+
+
+'''def change_parameter(enemy_height):
+    data = json.loads(open('jumpingParameter.json').read())
+    if str(enemy_height) in data:
+        data[str(enemy_height)] = int(data[str(enemy_height)]) - 1
+    else:
+        data[str(enemy_height)] = 100
+    
+    with open('jumpingParameter.json', 'w') as outfile:
+        json.dump(data, outfile)'''
